f3bea3a349acfb75def42a2d73cd0d7c1dbe24ac
Daedalus <daedalus@localhost.localdomain>
2019-11-12 19:52:52 -0600

    added assignment, worked on assignment

--- Intro_to_AI/Programming_Assignment_2/naive_digit_classifier.py
+++ Intro_to_AI/Programming_Assignment_2/naive_digit_classifier.py
@@ -1,29 +1,47 @@
 import numpy as np
 from string import whitespace
 
-def get_images(file):
+def get_images(data_file,label_file):
     images=[]
-    if file=='digitdata/trainingimages':
+    image_count=0
+    if data_file=='digitdata/trainingimages':
         image_count=5000
-    elif file=='digitdata/testimages':
+    elif data_file=='digitdata/testimages':
         image_count=1000
-    with open(file,'r') as f:
-        image=np.zeros([28,28])
-        for k in range(5000):
-            for i in range(28):
-                raw_data=f.readline()
-                for j in range(len(raw_data)):
-                    if raw_data[j]!='\n':
-                        if raw_data[j]!=' ':
-                            image[i,j]=1
-                        else:
-                            image[i,j]=0
-            images.append(image)
+    with open(data_file,'r') as data: 
+        with open(label_file,'r') as labels:
+            for k in range(image_count):
+            
+                images.append((get_label(labels),get_image(data)))
     return images    
 
+def get_image(data_file):
+    """get_image(data_file)
+    grabs a single image from the ascii data file
+    
+    Parameters
+    ----------
+        data_file: expects a txt file containing 28*28 ascii images
+        ! stops iter at start of next 28 line block.
+    """
+    image=np.zeros([28,28])
+    for i in range(28):
+        raw_data=data_file.readline()
+        for j in range(len(raw_data)):
+            if raw_data[j]!='\n':
+                if raw_data[j]!=' ':
+                    image[i,j]=1
+                else:
+                    image[i,j]=0
+    print_image(image)
+    return image
+        
+
 def print_image(image):
+    """
+    renders binary images in terminal, works with any N*N binary array
+    """
     print()
-    print(image.shape)
     for i in range(len(image)):
         print()
         for j in range(len(image[i])):
@@ -31,7 +49,17 @@ def print_image(image):
                 print(' ',end='')
             else:
                 print('#',end='')
+    print()
+
+def get_label(label_file):
+    """get_label(label_file)
+    grabs the next label from a data file
+    """
+    raw_data=label_file.readline()
+    value=int(raw_data)
+    print(value)
+    return value
 
 
 if __name__=="__main__":
-    get_images('digitdata/trainingimages')
+    get_images('digitdata/trainingimages','digitdata/traininglabels')
