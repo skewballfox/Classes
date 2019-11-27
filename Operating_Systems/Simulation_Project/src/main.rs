3d8198f53564cf50a70876284a1b3850754c1f4b
Daedalus <daedalus@localhost.localdomain>
2019-11-26 18:39:49 -0600

    starting the simulation project

--- Operating_Systems/Simulation_Project/src/main.rs
+++ Operating_Systems/Simulation_Project/src/main.rs
@@ -1,9 +1,39 @@
-macro_rules! random_gen_array
+/*
+    Author: Joshua Ferguson
+    Purpose: perform an insertion sort, and let me play around with rust for a bit
+
+ */
+use rand::{thread_rng, Rng};
+
+/*
+##############################   Macro Definitions   ##################################
+*/
+
+macro_rules! random_int_array
 {
-    ($size:ident,$random_array:ident)->
+    ($size:ident,$random_array:ident,$min:ident,$max:ident)=>
     (
 
+        let mut $random_array: [i32; $size]=[0;$size];
+        let mut rng = thread_rng();
+        for i in 0..$random_array.len()
+        {
+            
+            let random_value: i32 = rng.gen_range($min, $max);
+            
+            $random_array[i]=random_value;
+            
+        }
+    )
+
 }
-fn main() {
+
+/*
+##############################   Main Body   ##################################
+*/
+
+fn main() 
+{
     println!("Hello, world!");
 }
+
