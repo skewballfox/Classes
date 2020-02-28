6cec14b3f54227cca58ad778945121df7b3b0f5e
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2020-02-28 14:58:16 -0600

    lets test it out

--- parallel_processing/assignment_1/test.sh
+++ parallel_processing/assignment_1/test.sh
@@ -4,15 +4,38 @@ mpi_compile(){
 }
 
 mpi_run(){
-mpirun -c $2 ./$1
+mpirun -c 1 ./$1 $2 >> $3
+mpirun -c 2 ./$1 $2 >> $3
+mpirun -c 4 ./$1 $2 >> $3
+mpirun -c 8 ./$1 $2 >> $3
+
 }
 
-module load openmpi-2.0/gcc
-
 touch n1_data.md n2_data.md n3_data.md
-
+# compile the code
 gcc -lm serial_pi_estimator.c -o serial_pi_estimator 
 
-echo '# Data for round 1'
-./serial_pi_estimator 100 > n1_data.md 
-./serial_pi_estimator 10 > n2_data.md
+module load openmpi-2.0/gcc
+mpi_compile sp_mpi_pi_estimator.c
+mpi_compile cc_mpi_pi_estimator.c
+
+./serial_pi_estimator 100 >> n1_data.md 
+./serial_pi_estimator 10 >> n2_data.md
+./serial_pi_estimator >> n3_data.md
+
+echo -e "\n\nsingle point communicator output\n\n" >>n1_data.md
+echo -e "\n\nsingle point communicator output\n\n" >>n2_data.md
+echo -e "\n\nsingle point communicator output\n\n" >>n3_data.md
+
+mpi_run sp_mpi_pi_estimator 100 n1_data.md
+mpi_run sp_mpi_pi_estimator 10 n2_data.md
+mpi_run sp_mpi_pi_estimator 1 n3_data.md
+
+echo -e "\n\ncollective communication output\n\n" >>n1_data.md
+echo -e "\n\ncollective communication output\n\n" >>n2_data.md
+echo -e "\n\ncollective communication output\n\n" >>n3_data.md
+
+mpi_run cc_mpi_pi_estimator 100 n1_data.md
+mpi_run cc_mpi_pi_estimator 10 n2_data.md
+mpi_run cc_mpi_pi_estimator 1 n3_data.md
+
