6843716768ddf3297cb94a1531b198d11eaafc31
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2020-03-04 17:10:51 -0600

    working on a script to help with generating reports

---
+++ parallel_processing/report_gen.py
@@ -0,0 +1,13 @@
+#import regex
+import numpy as np
+
+pi_7_digits=3.141592
+
+assignment_directory="./assignment_1/"
+data_files=("n1_data.md", "n2_data.md", "n3_data.md")
+
+
+for data_file in data_files:
+    for line in  open(assignment_directory+data_file, 'r'):
+        if "pi:"
+        pi_estimate=
