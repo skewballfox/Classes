d8671aaee1fcb273ca21142427ebada35bf6cc42
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2020-03-09 14:25:38 -0500

    heading to class

--- object_oriented_programming/Project_1/Polynomial.java
+++ object_oriented_programming/Project_1/Polynomial.java
@@ -1,5 +1,17 @@
+import java.util.regex.Pattern;
+import java.util.Comparator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class Polynomial {
   Polynomial(string input){
-    regex.compile
+   if (in_test.matches(input)==True)
+       input.replace("-","+-");
+       for string
   }
+  private SortedSet<Polyterm> Polyterms=new TreeSet<>(
+      Comparator.comparing(Polyterm::getExponent));
+
+  // below can be used to test input
+  private Pattern in_test = Pattern.compile("([+|-]?([(\d+)|\d*(\.?=\d+)?])?(x(\^\d+)?)?)*");
 }
