f5492e28d02978db427ee982d9ae981fec64b629
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2020-04-30 20:27:11 -0500

    I just spent 3 hours playing around with scheduling

--- parallel_processing/assignment_3/OpenMP_pfor_pi_estimator.c
+++ parallel_processing/assignment_3/OpenMP_pfor_pi_estimator.c
@@ -9,7 +9,10 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
+
+#ifdef _OPENMP
 #include <omp.h>
+#endif
 struct point{
     double x;
     double y;
@@ -28,20 +31,27 @@ int main (int arg_count,char* arg_vector[])
     double start,finish;
     //necessary omp variables
     int threadCount=strtol(arg_vector[1],NULL,10);
-    int tosses_per_thread=total_tosses/threadCount;
+    //int tosses_per_thread=total_tosses/threadCount;
 
     //srand((unsigned)time(NULL));//initialize random seed
     //start timer
-    start=time(NULL);
+    # ifdef _OPENMP
+        start=omp_get_wtime();
+    # else
+        start=time(NULL);
+    # endif
 
     //do the math
-    circle_count=toss_darts(threadCount,tosses_per_thread);
+    circle_count=toss_darts(threadCount,total_tosses);
     //get finishing time
-    finish=time(NULL);
-
+    # ifdef _OPENMP
+        finish=omp_get_wtime();
+    # else
+        finish=time(NULL);
+    # endif
     long double pi=4*((long double)circle_count/(long double)(total_tosses));
                                                               //-((total_tosses%threadCount));
-    printf("circle count: %d\t--\ttotal_tosses: %d\t--\ttosses_per_thread: %d\t--\tthreadCount: %d\t--\tpi: %Lf\t--\t",circle_count,total_tosses,tosses_per_thread,threadCount, pi);
+    printf("circle count: %d\t--\ttotal_tosses: %d\t--\ttosses_per_thread: %d\t--\tthreadCount: %d\t--\tpi: %Lf\t--\t",circle_count,total_tosses,threadCount, pi);
     printf("elapsed time: %lf",finish-start);
     return 0;
 }
@@ -53,33 +63,28 @@ double distance(struct point point_1, struct point point_2){
     return sqrt(pow((point_2.x-point_1.x),2)+pow((point_2.y-point_1.y),2));
 }
 
-int toss_darts(int threadCount, int tosses_per_thread)
+int toss_darts(int threadCount, int tosses)
 {   
     /*function: pi_estimator
     purpose: estimate pi using monte carlo method by generating
     random points
     */
-    int global_circle_count=0;
-    int local_circle_count=0;
+    int i;
+    int circle_count=0;
     struct point center;
     center.x=1;
     center.y=1;
     struct point dart_toss;
-    unsigned int my_seed;
+    unsigned int my_seed=(unsigned)time(NULL);
     const double ranged_div = (double)RAND_MAX/2.0;
-    #pragma omp parallel default(none) \
-    private(dart_toss,my_seed,local_circle_count) shared(ranged_div,tosses_per_thread, center) \
-    num_threads(threadCount) reduction(+:global_circle_count)
-    {
-        my_seed=(unsigned)time(NULL)+omp_get_thread_num();//initialize random seed        
-        for (int i=0; i < tosses_per_thread; i++)
-        {
+    #pragma omp parallel for num_threads(threadCount) default(none) \
+    shared(tosses,center,ranged_div) private(my_seed,dart_toss) \
+    schedule(guided,1024) reduction(+:circle_count)
+    for (i=0; i < tosses; i++) {
             dart_toss.x=(double)rand_r(&my_seed)/ranged_div;
             dart_toss.y=(double)rand_r(&my_seed)/ranged_div;
             //printf("%e",dart_toss.x);
-            if (distance(center,dart_toss)<1.0) {local_circle_count++;}
-        }
-        global_circle_count+=local_circle_count;
+            if (distance(center,dart_toss)<1.0) {circle_count++;}
     }
-    return global_circle_count;
+    return circle_count;
 }
